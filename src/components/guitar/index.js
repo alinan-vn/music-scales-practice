d29e8dd1a37cea69a53f01f8c21c349df3a5a69e
Alejandro Linan <jandrolinan@gmail.com>
2020-07-15 15:45:49 -0500

    sheet music or tab can be toggled on or off for guitar

--- src/components/guitar/index.js
+++ src/components/guitar/index.js
@@ -4,19 +4,9 @@ class Guitar extends React.Component{
     constructor(){
         super()
         this.state = {
-            reading: {
-                sheetMusic: 'On',
-                tab: 'On'
-            },
-            buttons: {
-                defaultScale: 'guitar__scaleBtn',
-                selectedScale: 'guitar__scaleBtn-clicked'
-            }         
+            sheetMusic: 'On',
+            tab: 'On'
         }
-    }
-
-    resetScales = (btns) => {
-
     }
 
     scaleClick = (e) => {
@@ -38,13 +28,30 @@ class Guitar extends React.Component{
 
     scalesBtns = () => {
         const scaleKeys = ['G', 'Ab', 'A', 'Bb', 'C', 'Db', 'D', 'Eb', 'F', 'Gb']
-        // const defaultBtnClass = 'guitar__scaleBtn'
 
         return scaleKeys.map((note, ind) => {
             return(
                 <button id={ind} onClick={(e) => this.scaleClick(e)} name={note} className='guitar__scaleBtn'>{note}</button>
             )
         })
+    }
+
+    readingType = (e) => {
+        let onOff = e.target.innerText.split(' ')
+        onOff = onOff[onOff.length - 1]
+
+        const readType = e.target.name
+        if (onOff === 'On'){
+            this.setState({
+                ...this.state,
+                [readType]: 'Off'
+            })
+        } else {
+            this.setState({
+                ...this.state,
+                [readType]: 'On'
+            })
+        }
 
     }
 
@@ -53,8 +60,8 @@ class Guitar extends React.Component{
             <section className='guitar'>
                 <h1>Guitar Scales!</h1>
                 <div className='guitar__btns'>
-                    <button className='guitar__btn'>Sheet Music: {this.state.reading.sheetMusic}</button>
-                    <button className='guitar__btn'>Tab: {this.state.reading.tab}</button>
+                    <button onClick={(e) => this.readingType(e)} name='sheetMusic' className='guitar__btn'>Sheet Music: {this.state.sheetMusic}</button>
+                    <button onClick={(e) => this.readingType(e)} name='tab' className='guitar__btn'>Tab: {this.state.tab}</button>
                 </div>
                 <div className='guitar__btns'>
                     { this.scalesBtns() }
